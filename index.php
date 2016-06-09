1de32313f39c4e6acf8b25e44c2717214214704e
Lars Meissner <lmeissner@novomind.com>
2016-06-09 12:16:10 +0200

    test

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+		<title>Gurka</title>
+	</head>
+	<body id="home">
+<header>
+Header
+
+</header>
+<main>
+	<section class="layout right">
+		<h1>Headline</h1>
+
+				<aside>
+					<h3>aside</h3>
+				</aside>
+	
+				<article>
+					<h2>article</h2>
+				</article>
+	</section>
+</main>
+	</body>
+</html>
